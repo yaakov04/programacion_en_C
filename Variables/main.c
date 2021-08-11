d7fd484b8f2a8e5c4147f8edb5ca9ba0221c322c
Yaakov <dbrionesmtz@yahoo.com>
2021-08-11 15:54:24 -0500

    imprime varibles

--- Variables/main.c
+++ Variables/main.c
@@ -6,8 +6,19 @@ int main()
 {
     int integerA = 1;
     float floatA = 1.1;
-    char letterA ='ABC';
+    double doubleA =1.1;
+    char letterA ='A';
     bool frege = false;
+
+    //imprimir una varible que es entero
+    printf("Entero: %i", integerA);
+    //imprimir una varible que es float
+    printf("\nflotante: %f", floatA);
+    //imprimir una varible que es double
+    printf("\ndouble: %f", doubleA);
+    //imprimir una varible que es caracter
+    printf("\ncaracter: %c", letterA);
+
 
 
 
