60f1e191578e374b44b3f76abc9635793dacb461
Yaakov <dbrionesmtz@yahoo.com>
2021-08-18 18:50:35 -0500

    reto9

---
+++ reto9/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Imprimir '#' 5 veces\n");
+    int indice;
+    indice = 0;
+    while(indice<5){
+        printf("#\n");
+        indice++;
+    }
+    return 0;
+}
