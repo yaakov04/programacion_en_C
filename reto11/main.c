89be569d6b6b0451a40df1816a8ab48fed694b5d
Yaakov <dbrionesmtz@yahoo.com>
2021-08-18 20:35:29 -0500

    reto 11

---
+++ reto11/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Imprimir los primero 100 numeros\n");
+    int num =1;
+    do{
+        printf("%i\n", num);
+        num++;
+    }while(num <=100);
+    return 0;
+}
